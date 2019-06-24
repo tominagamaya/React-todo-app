34684c264c3d0f8b8feeb645a35ebaf15a7a2ee9
tominagamaya <maya.tominaga.9@gmail.com>
2019-06-24 15:36:59 +0900

    Add delete all with checked

--- src/components/App.js
+++ src/components/App.js
@@ -35,7 +35,7 @@ class App extends React.Component {
             </li>
           ))}
         </ul>
-        <button>完了済みを全て削除</button>
+        <button onClick={this.handleClickDeleteCompleted}>完了済みを全て削除</button>
       </div>
     );
   }
@@ -68,6 +68,10 @@ class App extends React.Component {
       return todo
     })
     this.setState({ todos: newTodos})
+  }
+  handleClickDeleteCompleted = () => {
+    const newTodos = this.state.todos.filter(({ completed }) => !completed)
+    this.setState({ todos: newTodos })
   }
 }
 
